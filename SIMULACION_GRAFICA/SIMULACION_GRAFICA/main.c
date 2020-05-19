44ff9b5af2e0264e987e9c48e7f816af47242b73
UrkoAT <urko.agirregomezkort@alumni.mondraon.edu>
2020-05-19 07:47:05 +0200

    File Manager v0.1

--- SIMULACION_GRAFICA/SIMULACION_GRAFICA/main.c
+++ SIMULACION_GRAFICA/SIMULACION_GRAFICA/main.c
@@ -1,8 +1,14 @@
-
+#include "sfd.h"
 
 
 int main() {
+	sfd_Options opt = {
+		.title = "Mapa kargatu",
+		.filter_name = "Mapak",
+		.filter = "*.json",
+	};
 
+	const char* filename = sfd_open_dialog(&opt);
 
 
 }
