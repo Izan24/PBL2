ccf17200aaac8f239eeb49fc5fd8d1d9f0f4eb39
UrkoAT <urko.agirregomezkort@alumni.mondraon.edu>
2020-05-19 13:18:26 +0200

    Structak preparatu

--- SIMULACION_GRAFICA/SIMULACION_GRAFICA/headers/general.h
+++ SIMULACION_GRAFICA/SIMULACION_GRAFICA/headers/general.h
@@ -1,6 +1,11 @@
 #ifndef GENERAL_H
 
 #define GENERAL_H
+#define MAX_STR 128
+#define MAX_CONNECTIONS 10
+
+typedef enum boolean{FALSE, TRUE}BOOL;
+typedef enum type{NODE, CROSSWALK, INTEREST}TYPE;
 
 #include <stdlib.h>
 #include <stdio.h>
