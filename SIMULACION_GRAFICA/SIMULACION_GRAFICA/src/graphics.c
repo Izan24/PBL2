272e694a1e83d9cc91396c776bb73dcbbcd398e8
UrkoAT <urko.agirregomezkort@alumni.mondraon.edu>
2020-05-29 10:30:40 +0200

    Little line tweak

--- SIMULACION_GRAFICA/SIMULACION_GRAFICA/src/graphics.c
+++ SIMULACION_GRAFICA/SIMULACION_GRAFICA/src/graphics.c
@@ -7,29 +7,37 @@
 
 void drawLineTo(LINE line, int red, int green, int blue)
 {
-    SDL_SetRenderDrawColor(rend, red, green, blue, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawLine(rend, line.x0, line.y0, line.x1, line.y1);
+	SDL_SetRenderDrawColor(rend, red, green, blue, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLine(rend, line.x0, line.y0, line.x1, line.y1);
 }
 
 
 SDL_Texture* bgInit(const char* img)
 {
-    SDL_Surface* surface;
-    SDL_Texture* bg;
+	SDL_Surface* surface;
+	SDL_Texture* bg;
 
-    bg = (SDL_Texture*)malloc(sizeof(SDL_Texture*));
+	bg = (SDL_Texture*)malloc(sizeof(SDL_Texture*));
 
-    surface = IMG_Load(img);
+	surface = IMG_Load(img);
 
-    bg = SDL_CreateTextureFromSurface(rend, surface);
+	bg = SDL_CreateTextureFromSurface(rend, surface);
 
-    return bg;
+	return bg;
 }
 
-void drawThiccLine(LINE line){
-    SDL_SetRenderDrawColor(rend, 255, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawLine(rend, line.x0+1, line.y0+1, line.x1+1, line.y1+1);
-    SDL_RenderDrawLine(rend, line.x0, line.y0, line.x1, line.y1);
-    SDL_RenderDrawLine(rend, line.x0-1, line.y0-1, line.x1-1, line.y1-1);
+void drawThiccLine(LINE line) { // Ty Orib
+	SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+	for (int j = -1; j < 2; j++) {
+		for (int k = -1; k < 2; k++) {
+			SDL_RenderDrawLine(rend,
+				line.x0 + j,
+				line.y0 + k,
+				line.x1 + j,
+				line.y1 + k);
+		}
+	}
 }
+
+
 
